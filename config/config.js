a53f03d0e966c00713e3185662a0d1ca63c5e012
maangeloatienza <maangeloatienza@gmail.com>
2018-11-30 21:12:26 +0800

    Add db config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 PORT = 8084,
 
-MYSQL_HOST = 'localhost';
-MYSQL_USER = 'root';
-MYSQL_PASSWORD = '12345';
-MYSQL_DATABASE = 'criminal_watch_db';
+MYSQL_HOST = 'localhost' || '85.10.205.173:3306';
+MYSQL_USER = 'root' || 'criminal_web';
+MYSQL_PASSWORD = '12345' || 'secret_password123';
+MYSQL_DATABASE = 'criminal_watch_db' || 'criminal_db';
 
 JWT_TOKEN      = 'secret_key';
 SECRET_KEY     = 'secret';
