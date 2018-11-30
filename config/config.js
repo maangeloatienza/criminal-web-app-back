bc096226382b1efb8424318d4610ac27dcf57542
maangeloatienza <maangeloatienza@gmail.com>
2018-11-30 21:19:40 +0800

    Fix database connection

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 PORT = 8084,
 
-MYSQL_HOST = 'localhost' || '85.10.205.173:3306';
+MYSQL_HOST = 'localhost' || '85.10.205.173';
 MYSQL_USER = 'root' || 'criminal_web';
 MYSQL_PASSWORD = '12345' || 'secret_password123';
 MYSQL_DATABASE = 'criminal_watch_db' || 'criminal_db';
