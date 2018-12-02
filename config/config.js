0c03a0eff5706d73d15e5a9669de77f2c8b3e625
maangeloatienza <maangeloatienza@gmail.com>
2018-12-02 17:42:58 +0800

    reconfigure config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
+DEVELOPMENT = true;
+
 PORT = 8084,
 
-MYSQL_HOST = '85.10.205.173' || 'locahost';
-MYSQL_USER = 'criminal_web' || 'root';
-MYSQL_PASSWORD = 'secret_password123'|| '12345';
-MYSQL_DATABASE = 'criminal_db' ||'criminal_watch_db';
+MYSQL_HOST = DEVELOPMENT ?process.env.MYSQL_HOST : '85.10.205.173';
+MYSQL_USER = DEVELOPMENT ?process.env.MYSQL_USER : 'criminal_web';
+MYSQL_PASSWORD = DEVELOPMENT ?process.env.MYSQL_PASSWORD : 'secret_password123';
+MYSQL_DATABASE = DEVELOPMENT ?process.env.MYSQL_DATABASE : 'criminal_db';
 
 JWT_TOKEN      = 'secret_key';
 SECRET_KEY     = 'secret';
